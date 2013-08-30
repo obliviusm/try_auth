743eca03ad0a47e690102502a686443f0dabc178
obliviusm <obliviusm@gmail.com>
2013-08-30 23:02:47 +0300

    Make a basic User model (including secure passwords)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryAuth::Application.config.secret_key_base = '1654c1f7364c01601fe44da4d212f8190a32732bc3f675f0b51c7ec3c98d67f997cb41d650a1f2d7caf9e23295e79cbd8a58bdaf10260fb8cce8c83d82bf2a88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TryAuth::Application.config.session_store :cookie_store, key: '_try_auth_session'
