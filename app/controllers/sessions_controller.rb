b7b609a41ba70190a4569967e2d3d6b83840061c
obliviusm <obliviusm@gmail.com>
2013-09-02 13:48:20 +0300

    cookies works. 8.2.5 Signin upon signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
 	def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user# Sign the user in and redirect to the user's show page.
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TryAuth::Application.routes.draw do
 	resources :users
 	resources :sessions, only: [:new, :create, :destroy]
+	match '/signup',  to: 'users#new',            via: 'get'
+  	match '/signin',  to: 'sessions#new',         via: 'get'
+  	match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
