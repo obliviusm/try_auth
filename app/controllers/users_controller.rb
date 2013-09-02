bd087218a11af842ca3727dc76de5d0dea8655bf
obliviusm <obliviusm@gmail.com>
2013-09-02 14:21:06 +0300

    add Requiring signed-in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	  before_action :signed_in_user, only: [:show]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -18,8 +20,14 @@ class UsersController < ApplicationController
 	    end
   	end
 
-  	def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  	private
+
+	  	def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
+
+		def signed_in_user
+	      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	    end
 end
